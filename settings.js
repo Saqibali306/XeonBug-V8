0c6e462f1d4b3e20ba42a07dfce3e6f6df51fc43
Saqibali306 <138368204+Saqibali306@users.noreply.github.com>
2024-09-26 00:43:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "923181862950"
+global.ownername = "Saqib Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
